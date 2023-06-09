da47953c5a9c2404eb95dfcdded0d35f9d1480b5
Andreas K. Petersen <AndreasKPetersen@hotmail.com>
2023-06-09 14:59:25 +0200

    1.8: unicafe step3 + Last commit was mostly step3

--- part01/unicafe/src/App.js
+++ part01/unicafe/src/App.js
@@ -24,18 +24,18 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  // const [total, setTotal] = useState(0)
-  // const [average, setAverage] = useState(0)
-  // const [positive, setPositive] = useState(0)
 
   return (
     <div>
+
       <h1>give feedback</h1>
         <Button handleClick={() => setGood(good + 1)} text="good" />
         <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
         <Button handleClick={() => setBad(bad + 1)} text="bad" />
+
       <h1>statistics</h1>
         <Statistics good={good} neutral={neutral} bad={bad} />
+
     </div>
   )
 }
