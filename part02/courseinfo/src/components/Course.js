be6379c13ce120ef39edc2e96dbda3912e86e38d
Andreas K. Petersen <AndreasKPetersen@hotmail.com>
2023-06-13 09:42:11 +0200

    2.4: Course information step9 + 2.5: separate module

--- part02/courseinfo/src/components/Course.js
+++ part02/courseinfo/src/components/Course.js
@@ -1,22 +1,45 @@
-function Course( {course} ) {
-    const courseName = course.name
-    const parts = course.parts
-    
+const Header = ( {course} ) => {
+    return (
+        <h3>{course.name}</h3>
+    )
+}
+
+const Content = ( {parts} ) => {
+    return (
+        <div>
+            {parts.map(part => 
+                <p key={part.id}>{part.name} {part.exercises}</p>
+            )}
+        </div>
+    )
+}
+
+const Total = ( {parts} ) => {
     const total = parts.reduce((sum, part) => {
         return (
             sum + part.exercises
         )
     }, 0)
+    
+    return (
+        <b>total of {total} exercises</b>
+    )
+}
+
+const Course = ( {courses} ) => {
 
     return (
         <div>
-            <h1>{courseName}</h1>
-            
-            {parts.map(x => 
-                <div key={x.id}>{x.name} {x.exercises}</div>
+            <h1>Web development curriculum</h1>
+
+            {courses.map(course =>
+                <div key={course.id}>
+                    <Header course={course} />
+                    <Content parts={course.parts} />
+                    <Total parts={course.parts} />
+                </div>
             )}
 
-            total of {total} exercises
         </div>
     )
 }
