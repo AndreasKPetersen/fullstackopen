e377c6c53694524aab48ad9763b8d26004183da5
Andreas K. Petersen <AndreasKPetersen@hotmail.com>
2023-07-11 05:37:26 +0200

    3.12: Command-line database

--- part03/phonebook/mongo.js
+++ part03/phonebook/mongo.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
 
-if (process.argv.length < 3) {
+const argc = process.argv.length
+
+if (argc !== 3 && argc !== 5) {
   console.log('Incorrect usage: node mongo.js password')
   process.exit(1)
 }
@@ -8,8 +10,7 @@ if (process.argv.length < 3) {
 const password = process.argv[2]
 
 const url =
-  `mongodb+srv://andreaskpetersen:${password}@phonebook.ggcvdpp.mongodb.net
-  `
+  `mongodb+srv://andreaskpetersen:${password}@phonebook.ggcvdpp.mongodb.net/personApp`
 
 mongoose.set('strictQuery',false)
 mongoose.connect(url)
@@ -21,7 +22,30 @@ const personSchema = new mongoose.Schema({
 
 const Person = mongoose.model('Person', personSchema)
 
-person.save().then(result => {
-  console.log('person saved!')
-  mongoose.connection.close()
-})
+if (argc === 3) {
+  Person.find({}).then(result => {
+    console.log('phonebook:')
+    result.forEach(person => {
+      console.log(person)
+    })
+    mongoose.connection.close()
+  })
+}
+
+if (argc === 5) {
+  const name = process.argv[3]
+  const number = process.argv[4]
+
+  const person = new Person({
+    name: name,
+    number: number,
+  })
+
+  person.save().then(result => {
+    console.log(`added ${name} number ${number} to phonebook`)
+    mongoose.connection.close()
+  })
+}
+
+
+
