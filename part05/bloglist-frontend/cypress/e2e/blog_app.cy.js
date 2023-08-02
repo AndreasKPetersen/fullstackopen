fe6ae78c2494dfd902766942da49fc34ae36d04f
Andreas K. Petersen <AndreasKPetersen@hotmail.com>
2023-08-02 13:40:07 +0200

    5.19: bloglist end to end testing, step3

--- part05/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part05/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -19,18 +19,35 @@ describe('Blog app', function() {
 
   describe('Login', function() {
     it('succeeds with correct credentials', function() {
-      cy.get('.username').type(user.username)
-      cy.get('.password').type(user.password)
-      cy.get('.loginButton').click()
+      cy.get('input.username').type(user.username)
+      cy.get('input.password').type(user.password)
+      cy.get('button.loginButton').click()
 
       cy.contains(`${user.username} logged in`)
     })
 
     it('fails with wrong credentials', function() {
-      cy.get('.username').type(user.username)
-      cy.get('.loginButton').click()
+      cy.get('input.username').type(user.username)
+      cy.get('button.loginButton').click()
 
-      cy.get('.errorMessage')
+      cy.get('div.errorMessage')
+    })
+  })
+
+  describe('When logged in', function() {
+    beforeEach(function() {
+      cy.get('input.username').type(user.username)
+      cy.get('input.password').type(user.password)
+      cy.get('button.loginButton').click()
+    })
+
+    it('A blog can be created', function() {
+      cy.get('input.title').type('title')
+      cy.get('input.author').type('author')
+      cy.get('input.url').type('url')
+      cy.get('button.createButton').click()
+
+      cy.get('div.blog')
     })
   })
 })
