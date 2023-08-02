65aa096e4167ba1eec14a936c3108bceefde3c10
Andreas K. Petersen <AndreasKPetersen@hotmail.com>
2023-08-02 21:05:56 +0200

    5.22: bloglist end to end testing, step6

--- part05/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part05/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -2,7 +2,7 @@ describe('Blog app', function() {
   let user1, user2
 
   beforeEach(function() {
-    cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    cy.request('POST', `${Cypress.env('BACKEND')}/testing/reset`)
     user1 = {
       username: 'username1',
       name: 'name1',
@@ -13,9 +13,9 @@ describe('Blog app', function() {
       name: 'name2',
       password: 'password2'
     }
-    cy.request('POST', 'http://localhost:3003/api/users', user1)
-    cy.request('POST', 'http://localhost:3003/api/users', user2)
-    cy.visit('http://localhost:3000')
+    cy.request('POST', `${Cypress.env('BACKEND')}/users`, user1)
+    cy.request('POST', `${Cypress.env('BACKEND')}/users`, user2)
+    cy.visit('')
   })
 
   it('front page can be opened and login form is shown by default', function() {
@@ -41,10 +41,12 @@ describe('Blog app', function() {
   })
 
   describe('When logged in', function() {
+    let credentialsUser1, credentialsUser2
+    
     beforeEach(function() {
-      cy.get('input.username').type(user1.username)
-      cy.get('input.password').type(user1.password)
-      cy.get('button.loginButton').click()
+      credentialsUser1 = { username: user1.username, password: user1.password }
+      credentialsUser2 = { username: user2.username, password: user2.password }
+      cy.login(credentialsUser1)
     })
 
     it('A blog can be created', function() {
@@ -55,32 +57,50 @@ describe('Blog app', function() {
 
       cy.get('div.blog')
     })
+
+    describe('and multiple blogs are created', function() {
+      let blog1, blog2, blog3
+
+      beforeEach(function() {
+        blog1 = {
+          title: 'title1',
+          author: 'author1',
+          url: 'url1'
+        }
+        blog2 = {
+          title: 'title2',
+          author: 'author2',
+          url: 'url2'
+        }
+        blog3 = {
+          title: 'title3',
+          author: 'author3',
+          url: 'url3'
+        }
+
+        cy.createBlog(blog1)
+        cy.createBlog(blog2)
+        cy.createBlog(blog3)
+      })
+
+      it('a blog can be liked', function() {
+        cy.contains(blog1.title).parent().find('.visibilityButton').click()
+        cy.get('.likeButton').click()
+      })
+
+      it('a blog can be deleted', function() {
+        cy.contains(blog1.title).parent().find('.visibilityButton').click()
+        cy.get('.deleteButton').click()
+        cy.get('html').should('not.contain', blog1.title)
+      })
+
+      it('a blog can only be deleted by user who created blog', function() {
+        cy.contains('logout').click()
+        cy.login(credentialsUser2)
+        cy.contains(blog1.title).parent().find('.visibilityButton').click()
+        cy.get('html').should('not.contain', '.deleteButton')
+      })
+    })
   })
 
-  describe('When logged in and a blog is created', function() {
-    beforeEach(function() {
-      cy.get('input.username').type(user1.username)
-      cy.get('input.password').type(user1.password)
-      cy.get('button.loginButton').click()
-
-      cy.get('input.title').type('title')
-      cy.get('input.author').type('author')
-      cy.get('input.url').type('url')
-      cy.get('button.createButton').click()
-
-      cy.get('button.visibilityButton').click()
-    })
-
-    it('a blog can be liked', function() {
-      cy.get('.likes').should('contain', '0')
-      cy.get('button.likeButton').click()
-      cy.get('.likes').should('contain', '1')
-    })
-
-    it('a blog can be deleted', function() {
-      cy.get('button.deleteButton').click()
-      cy.get('html')
-        .should('not.contain', 'title')
-    })
-  })
 })
