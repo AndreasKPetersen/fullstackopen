4216526ee6d644d06b3f3cb06d44ce460e437859
Andreas K. Petersen <AndreasKPetersen@hotmail.com>
2023-08-02 08:20:44 +0200

    5.14: Blog list tests, step2

--- part05/bloglist-frontend/src/components/Blog.test.js
+++ part05/bloglist-frontend/src/components/Blog.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 describe('<Blog />', () => {
@@ -21,18 +22,41 @@ describe('<Blog />', () => {
     username: 'username'
   }
 
+  const updateLikesMockHandler = jest.fn()
+  const deleteBlogMockHandler = jest.fn()
+
   beforeEach(() => {
-    component = render(<Blog blog={blog} user={user} />)
+
+
+    component = render(<Blog
+      blog={blog}
+      updateLikes={updateLikesMockHandler}
+      user={user}
+      deleteBlog={deleteBlogMockHandler}
+    />)
   })
 
   test('renders title and author, but not URL or number of likes by default', () => {
+    const blogContainer = component.container.querySelector('.blog')
+    expect(blogContainer).toHaveTextContent(blog.title)
+    expect(blogContainer).toHaveTextContent(blog.author)
+    expect(blogContainer).not.toHaveTextContent(blog.url)
+    expect(blogContainer).not.toHaveTextContent(blog.likes)
+    expect(blogContainer).not.toHaveTextContent(blog.user.username)
+  })
 
-    const div = component.container.querySelector('.blog')
-    expect(div).toHaveTextContent(blog.title)
-    expect(div).toHaveTextContent(blog.author)
-    expect(div).not.toHaveTextContent(blog.url)
-    expect(div).not.toHaveTextContent(blog.likes)
-    expect(div).not.toHaveTextContent(blog.user.username)
+  test('URL and number of likes shown when button controlling details clicked', async () => {
+    const user = userEvent.setup()
+    const button = component.container.querySelector('.visibilityButton')
+    await user.click(button)
+
+    const blogDetailsContainer = component.container.querySelector('.blogDetails')
+    expect(blogDetailsContainer).toBeDefined()
+
+    const blogContainer = component.container.querySelector('.blog')
+    expect(blogContainer).toHaveTextContent(blog.url)
+    expect(blogContainer).toHaveTextContent(blog.likes)
+    expect(blogContainer).toHaveTextContent(blog.user.username)
   })
 })
 
