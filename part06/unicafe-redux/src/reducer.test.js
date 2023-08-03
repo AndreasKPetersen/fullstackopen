f1944acb814c58702f7b72533a666f4d38ed1156
Andreas K. Petersen <AndreasKPetersen@hotmail.com>
2023-08-03 10:36:29 +0200

    6.1: unicafe revisited, step1 + 6.2: unicafe revisited, step2

--- part06/unicafe-redux/src/reducer.test.js
+++ part06/unicafe-redux/src/reducer.test.js
@@ -1,5 +1,7 @@
-import deepFreeze from 'deep-freeze'
-import counterReducer from './reducer'
+const deepFreeze = require('deep-freeze') 
+const counterReducer = require('./reducer')
+// import counterReducer from './reducer'
+// import deepFreeze from 'deep-freeze'
 
 describe('unicafe reducer', () => {
   const initialState = {
@@ -28,6 +30,55 @@ describe('unicafe reducer', () => {
     const newState = counterReducer(state, action)
     expect(newState).toEqual({
       good: 1,
+      ok: 0,
+      bad: 0
+    })
+  })
+
+  test('ok is incremented', () => {
+    const action = {
+      type: 'OK'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 1,
+      bad: 0
+    })
+  })
+
+  test('bad is incremented', () => {
+    const action = {
+      type: 'BAD'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 1
+    })
+  })
+
+  test('reset is working', () => {
+    const action = {
+      type: 'ZERO'
+    }
+    const state = {
+      good: 5,
+      ok: 5,
+      bad: 5
+    }
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
       ok: 0,
       bad: 0
     })
