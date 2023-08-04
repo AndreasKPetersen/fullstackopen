6a4d973cdb765a6e961fab14ad82d3ca1d37c2e6
Andreas K. Petersen <AndreasKPetersen@hotmail.com>
2023-08-04 09:12:20 +0200

    6.3: anecdotes, step1

--- part06/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part06/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -19,11 +19,29 @@ const asObject = (anecdote) => {
 
 const initialState = anecdotesAtStart.map(asObject)
 
+export const incrementVote = (id) => {
+  return {
+    type: 'VOTE',
+    payload: { id }
+  }
+}
+
 const reducer = (state = initialState, action) => {
   console.log('state now: ', state)
   console.log('action', action)
 
-  return state
+  switch (action.type) {
+    case 'VOTE':
+      const id = action.payload.id
+      const anecdoteToChange = state.find(anecdote => anecdote.id === id)
+      const changedAnecdote = {
+        ...anecdoteToChange, votes: anecdoteToChange.votes + 1
+      }
+      return state.map(anecdote => anecdote.id !== id ? anecdote : changedAnecdote)
+    
+    default:
+      return state
+  }
 }
 
 export default reducer
