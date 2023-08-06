b34e4c76ad13e5162d245ae2d2d41c6179e9d302
Andreas K. Petersen <AndreasKPetersen@hotmail.com>
2023-08-06 21:49:57 +0200

    6.16 Anecdotes and the backend, step3

--- part06/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part06/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit'
+import anecdoteService from '../services/anecdotes'
 
 const anecdotesAtStart = {
   anecdotes: [
@@ -54,4 +55,12 @@ const anecdoteSlice = createSlice({
 })
 
 export const { createAnecdote, incrementAnecdoteVote, appendAnecdote, setAnecdotes } = anecdoteSlice.actions
+
+export const initializeAnecdotes = () => {
+  return async dispatch => {
+    const anecdotes = await anecdoteService.getAll()
+    dispatch(setAnecdotes(anecdotes))
+  }
+}
+
 export default anecdoteSlice.reducer
