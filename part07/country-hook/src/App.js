cb92328eedb486fcabba60821125372ae5fd6bd4
Andreas K. Petersen <AndreasKPetersen@hotmail.com>
2023-08-08 11:29:56 +0200

    7.7: country hook

--- part07/country-hook/src/App.js
+++ part07/country-hook/src/App.js
@@ -18,7 +18,32 @@ const useField = (type) => {
 const useCountry = (name) => {
   const [country, setCountry] = useState(null)
 
-  useEffect(() => {})
+  useEffect(() => {
+    if (!name) {
+      return
+    }
+
+    const findCountry = async () => {
+      try {
+        axios.get(`https://studies.cs.helsinki.fi/restcountries/api/name/${name}`)
+        .then(response => {
+          setCountry( {
+            found: true,
+            data: {
+              name: response.data.name.common,
+              capital: response.data.capital[0],
+              population: response.data.population,
+              flag: response.data.flags.png
+            }
+          } )
+        })
+      } catch (error) {
+        setCountry( {found: false, data: null} )
+      }
+    }
+
+    findCountry()
+  }, [name])
 
   return country
 }
@@ -35,7 +60,6 @@ const Country = ({ country }) => {
       </div>
     )
   }
-
   return (
     <div>
       <h3>{country.data.name} </h3>
