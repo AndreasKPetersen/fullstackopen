e8acf00aed431bfb2b78acae1564c5ac46d6055d
Andreas K. Petersen <AndreasKPetersen@hotmail.com>
2023-08-16 12:17:36 +0200

    9.5 WebBMI

--- part09/bmi/bmiCalculator.ts
+++ part09/bmi/bmiCalculator.ts
@@ -1,4 +1,8 @@
 const calculateBmi = (height: number, weight: number): string => {
+  if (!height || !weight) {
+    return "Please enter height and weight to get bmi"
+  }
+
   const bmi = weight / (height * 0.01) ** 2
 
   if (bmi < 18.5) {
@@ -14,8 +18,4 @@ const calculateBmi = (height: number, weight: number): string => {
   }
 }
 
-const height: number = Number(process.argv[2])
-const weight: number = Number(process.argv[3])
-console.log(calculateBmi(height, weight))
-
-// console.log(calculateBmi(180, 74)) // should print Normal (healthy weight)
+export default calculateBmi
--- part09/bmi/index.ts
+++ part09/bmi/index.ts
@@ -1,8 +1,16 @@
 import express from "express"
+import calculateBmi from "./bmiCalculator"
 const app = express()
 
 app.get("/hello", (_req, res) => {
   res.send("Hello Full Stack!")
+})
+
+app.get("/bmi", (req, res) => {
+  const weight: number = Number(req.query.weight)
+  const height: number = Number(req.query.height)
+  const bmi: string = calculateBmi(height, weight)
+  res.send({ weight: weight, height: height, bmi: bmi })
 })
 
 const PORT = 3011
