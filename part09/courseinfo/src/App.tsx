e6a50ffc804fc5deff58d5a74d48a3ff5b8d4d85
Andreas K. Petersen <AndreasKPetersen@hotmail.com>
2023-08-17 13:46:57 +0200

    9.14

--- part09/courseinfo/src/App.tsx
+++ part09/courseinfo/src/App.tsx
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+interface CourseParts {
+  name: string;
+  exerciseCount: number;
+}
 
-function App() {
+const Header = ({ courseName }: { courseName: string }) => {
+  return <h1>{courseName}</h1>;
+};
+
+const Content = ({ courseParts }: { courseParts: CourseParts[] }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {courseParts.map((coursePart) => {
+        return (
+          <p key={coursePart.name}>
+            {coursePart.name} {coursePart.exerciseCount}
+          </p>
+        );
+      })}
     </div>
   );
-}
+};
+
+const Total = ({ courseParts }: { courseParts: CourseParts[] }) => {
+  return (
+    <p>
+      Number of exercises{" "}
+      {courseParts.reduce((carry, part) => carry + part.exerciseCount, 0)}
+    </p>
+  );
+};
+
+const App = () => {
+  const courseName = "Half Stack application development";
+  const courseParts = [
+    {
+      name: "Fundamentals",
+      exerciseCount: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exerciseCount: 7,
+    },
+    {
+      name: "Deeper type usage",
+      exerciseCount: 14,
+    },
+  ];
+
+  return (
+    <div>
+      <Header courseName={courseName} />
+      <Content courseParts={courseParts} />
+      <Total courseParts={courseParts} />
+    </div>
+  );
+};
 
 export default App;
